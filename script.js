d8b8797ed66753ca532e96289eb14bad728215e3
ninakd21 <nina.k.davis2@gmail.com>
2021-09-16 19:47:15 -0500

    final updates

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-var currentDay = document.querySelector("#currentDay");
 
-// display current day
-function setDay() {
-    var day = moment().format("dddd, MMM Do YYYY");
-
-    currentDay.innerHTML = day;
-};
 
 
 $(document).ready(function() {
 
-function hourUpdater() {
-    var currentHour = moment().hours();
+    $('.saveBtn').on('click', function() {
+        var value = $(this)
+        .siblings('.description').val();
+        var time = $(this)
+        .parent()
+        .attr('id');
+    
+        localStorage.setItem(time, value);
 
-    $('.time-block').each(function() {
-        var blockHour = parseInit(
+        // $('.notification').addClass('show');
+    
+        // setTimeout(function() {
+        //     $('.notification').removeClass('show');
+    
+        // }, 500)
+    });
+
+function hourUpdater() {
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function() {
+        var blockHour = parseInt(
             $(this)
             .attr("id")
-            .split("hour") [1]
+            .split("-")[1]
         );
         if (blockHour < currentHour) {
             $(this).removeClass("future");
@@ -34,29 +44,20 @@ function hourUpdater() {
         }
     })
 };
-$('.saveBtn').on('click', function() {
-    var value = $(this)
-    .siblings('.description').val
-    var time = $(this)
-    .parent()
-    .attr('id');
 
-    localStorage.setItem(time, value);
+var interval = setInterval(hourUpdater, 1500);
 
-})
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-$('hour-9 .description').val(localStorage.getItem('hour-9'));
-$('hour-10 .description').val(localStorage.getItem('hour-10'));
-$('hour-11 .description').val(localStorage.getItem('hour-11'));
-$('hour-12 .description').val(localStorage.getItem('hour-12'));
-$('hour-13 .description').val(localStorage.getItem('hour-13'));
-$('hour-14 .description').val(localStorage.getItem('hour-14'));
-$('hour-15 .description').val(localStorage.getItem('hour-15'));
-$('hour-16 .description').val(localStorage.getItem('hour-16'));
-$('hour-17 .description').val(localStorage.getItem('hour-17'));
 
-hourUpdater();
-
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
 });
 
-setDay();
